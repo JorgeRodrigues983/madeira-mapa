d339d56cd4882b55675ce08859655755c777b009
JorgeRodrigues983 <jorgerodrigues0983@gmail.com>
2025-11-05 21:46:01 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-// service worker simples
 self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  // permite que a app funcione mesmo que falhe o pedido
-  event.respondWith(
-    fetch(event.request).catch(() => new Response('Offline'))
-  );
+  event.respondWith(fetch(event.request));
 });
